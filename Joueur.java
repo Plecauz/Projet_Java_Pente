6da6a6a09033eef2decca42d0720f77c509054bb
LaFougere59 <51946971+LaFougere59@users.noreply.github.com>
2021-05-25 15:32:07 +0200

    Update Joueur.java
    Changement listeDePions, ajout d'une méthode listedePions et modification de la méthode toString

--- Joueur.java
+++ Joueur.java
@@ -8,18 +8,31 @@ public class Joueur {
     int numero;
     boolean tourdejouer;
     
-    
+    //METHODES
+
+    //Constructeur par défaut
     public Joueur(){
-        this.nom = nom;
-        this.numero = numero;
+        this.nom = "nom inconnu";
+        this.numero = 0;
         this.tourdejouer = false;
         
         this.listeDePions= new Pion[35] ;
         
     }
-
+    //Methode qui determine le joueur qui joue en premier
+    public static int JoueurStart(){
+        double rand = Math.random();
+        int num = 0;
+        if(rand >= 0.5){
+            num = 2;
+        }
+        else{
+            num = 1;
+        }
+        return num;
+    }
     
-  
+    //Methode qui initialise la liste de pions avec leur constructeur par défaut
     public void listedePions(){
         for (int i = 0 ; i<listeDePions.length-1; i++){
             this.listeDePions[i]= new Pion();
@@ -27,13 +40,8 @@ public class Joueur {
     }
 
   
-    /*
-    public void placerPion(int index,Pion pion){
-        this.listeDePions[index] = pion;
-        
-    }
-    */
-
+  
+    //Méthode qui retourne le nom du joueur, son numero de joueur, son nombre de pions et si c'est à lui de jouer
     public String toString(){
         return (this.nom+"est le joueur numero "+ this.numero+" et possede "+ this.listeDePions.length + " " + "pions" "et son tour de jouer est sur " +this.tourdejouer);
         
