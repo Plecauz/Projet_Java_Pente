918d8726a2a5f4413ff94cc6407fcf678c134f2c
Plecauz <50369990+Plecauz@users.noreply.github.com>
2021-05-25 14:03:38 +0200

    Ajout commentaire

--- Pion.java
+++ Pion.java
@@ -1,25 +1,31 @@
 public class Pion {
     private int x;
     private int y;
+
+    //Constructeur par défaut
     public Pion(){
         this.x = 0;
         this.y = 0;
     }
 
+    //Constructeur par paramètres
     public Pion(int x,int y){
         this.x = x;
         this.y = y;
     }
 
+    //Modifie la position
     public void setPosition(int x, int y){
         this.x = x;
         this.y = y;
     }
 
+    //Retourne la position x
     public int getX(){
         return this.x;
     }
 
+    //Retourne la position y
     public int getY(){
         return this.y;
     }
