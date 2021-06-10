0a4aeef921786ce3f34bccedb2a39bfa42fe46ed
Plecauz <50369990+Plecauz@users.noreply.github.com>
2021-06-10 11:01:22 +0200

    update

--- Jeu.java
+++ Jeu.java
@@ -56,4 +56,24 @@ public class Jeu {
             }
         }
     }
+
+        public void victoire(int numJoueur) {
+    
+            String joueur = String.valueOf(numJoueur);
+            int largeur = 1080;
+            int hauteur = 720;
+            Font Calibri = new Font("Calibri", Font.TYPE1_FONT, 40);
+            Couleur texte = new Couleur(153,38,0);
+            Couleur cadre = new Couleur(166,166,166);
+            Fenetre f = new Fenetre("Jeu de Pente", largeur, hauteur);
+    
+            f.ajouter(new Rectangle(cadre, new Point(300,500), new Point(780,600), true));
+            f.ajouter(new Texte(texte, "Felicitations Joueur " + joueur + " !", Calibri, new Point ((980/2)+50,500+50)));
+    
+            f.ajouter(new Rectangle(cadre, new Point(300,425), new Point(780,500), true));
+            f.ajouter(new Texte(texte, "Vous avez gagne !!!", Calibri, new Point ((980/2)+50,500)));
+    
+            f.rafraichir();
+        }
+    }
 }
